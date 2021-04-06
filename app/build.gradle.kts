9d7efa710d1b8de456c38520b1aee286eae355fc
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-04-06 21:57:50 +0000

    Added TravisCI config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,4 +30,9 @@ android{
             )
         }
     }
-   
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        
