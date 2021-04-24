d69a3cf2bc7ab0fbb88953da4be9fe742e5f6715
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-04-24 11:20:01 +0000

    Removed console.logs

--- app/src/androidTest/java/pawel/hn/coinmarketapp/database/CoinDaoTest.kt
+++ app/src/androidTest/java/pawel/hn/coinmarketapp/database/CoinDaoTest.kt
@@ -2,4 +2,7 @@ package pawel.hn.coinmarketapp.database
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
-imp
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.asser
