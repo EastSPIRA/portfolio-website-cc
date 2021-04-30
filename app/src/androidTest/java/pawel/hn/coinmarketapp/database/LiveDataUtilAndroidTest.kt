ccb5aabbac70163db7720c83144df8755c82dbab
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-04-30 08:09:01 +0000

    Spanish Translation

--- app/src/androidTest/java/pawel/hn/coinmarketapp/database/LiveDataUtilAndroidTest.kt
+++ app/src/androidTest/java/pawel/hn/coinmarketapp/database/LiveDataUtilAndroidTest.kt
@@ -4,4 +4,10 @@ import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.CountDownLatch
-im
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+/**
+ * Gets the value of a [LiveData] or waits for it to have one, with a timeout.
+ *
+ 
