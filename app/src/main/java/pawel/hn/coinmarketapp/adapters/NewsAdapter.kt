b573ec249662c7b179f90d35c55968fa08e9985a
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-05-28 10:22:03 +0000

    net: update consumers of MSG_MORE to recognize MSG_SENDPAGE_NOTLAST

--- app/src/main/java/pawel/hn/coinmarketapp/adapters/NewsAdapter.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/adapters/NewsAdapter.kt
@@ -1,1 +1,6 @@
-p
+package pawel.hn.coinmarketapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.gl
