368f99df94f42e985ae4921ebf4a911550fb98da
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-05-28 11:49:09 +0000

    Gem updates

--- app/src/main/java/pawel/hn/coinmarketapp/adapters/NewsAdapter.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/adapters/NewsAdapter.kt
@@ -3,4 +3,10 @@ package pawel.hn.coinmarketapp.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.gl
+import com.bumptech.glide.Glide
+import com.prof.rssparser.Article
+import pawel.hn.coinmarketapp.databinding.ItemNewsBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+cla
