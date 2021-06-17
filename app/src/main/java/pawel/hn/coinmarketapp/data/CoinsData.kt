7a966759ac9f3fb25f5651c6d3f44c3db3d0ed0f
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-06-17 16:01:19 +0000

    Delete .classpath

--- app/src/main/java/pawel/hn/coinmarketapp/data/CoinsData.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/data/CoinsData.kt
@@ -2,4 +2,11 @@ package pawel.hn.coinmarketapp.data
 
 import androidx.lifecycle.LiveData
 import pawel.hn.coinmarketapp.database.Coin
-import pawel.hn.coinmarketapp.d
+import pawel.hn.coinmarketapp.database.CoinDao
+import pawel.hn.coinmarketapp.database.Notifications
+import javax.inject.Inject
+
+
+class CoinsData @Inject constructor(private val coinDao: CoinDao) {
+
+    fun g
