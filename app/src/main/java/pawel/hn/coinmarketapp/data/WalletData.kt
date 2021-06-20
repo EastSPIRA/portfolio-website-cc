7188d885124bd3d58b5131803ab09d2ac36fcf5c
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-06-20 22:31:24 +0000

    Bump version for release [minor]

--- app/src/main/java/pawel/hn/coinmarketapp/data/WalletData.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/data/WalletData.kt
@@ -3,4 +3,11 @@ package pawel.hn.coinmarketapp.data
 import pawel.hn.coinmarketapp.database.Coin
 import pawel.hn.coinmarketapp.database.Wallet
 import pawel.hn.coinmarketapp.database.WalletDao
-import javax.inject.
+import javax.inject.Inject
+
+
+class WalletData @Inject constructor(private val walletDao: WalletDao) {
+
+    val wallet = walletDao.getWallet()
+
+    suspend fun addToWallet(newCoin:
