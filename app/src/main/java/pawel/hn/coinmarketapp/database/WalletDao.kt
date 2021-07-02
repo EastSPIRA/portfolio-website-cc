0ad844ce0e644b8d7ae8b77886b9401139b2dce0
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-07-02 10:10:43 +0000

    Add Docs

--- app/src/main/java/pawel/hn/coinmarketapp/database/WalletDao.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/database/WalletDao.kt
@@ -1,4 +1,11 @@
 package pawel.hn.coinmarketapp.database
 
 import androidx.lifecycle.LiveData
-i
+import androidx.room.*
+
+
+@Dao
+interface WalletDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE, entity = Wallet::class)
+    suspend fun insertIntoWall
