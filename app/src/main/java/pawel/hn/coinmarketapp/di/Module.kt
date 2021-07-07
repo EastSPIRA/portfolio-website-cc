54625990269de79642c93c9206d0cf44d8dcbb89
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-07-07 15:44:42 +0000

    :dog:

--- app/src/main/java/pawel/hn/coinmarketapp/di/Module.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/di/Module.kt
@@ -5,4 +5,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonCom
+import dagger.hilt.components.SingletonComponent
+import pawel.hn.coinmarketapp.api.CoinApi
+import pawel.hn.coinmarketapp.data.CoinsData
+import pawel.hn.coinmarketapp.data.RemoteData
+import pawel.hn.coinmarketapp.data.W
