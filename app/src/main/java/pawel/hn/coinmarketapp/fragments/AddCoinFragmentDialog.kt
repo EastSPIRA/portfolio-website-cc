41ca9a3c6ece21f00df0d2cb7529f3628327ae1c
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-07-18 19:12:07 +0000

    Stage Dev iOS  #Startup Mobile Big Data (H/F) en Stage à Paris

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/AddCoinFragmentDialog.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/AddCoinFragmentDialog.kt
@@ -5,4 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
-import android.w
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPo
