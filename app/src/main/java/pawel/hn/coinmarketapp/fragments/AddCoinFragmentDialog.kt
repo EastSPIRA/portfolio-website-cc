1a725bdb5c5e7e4950f84fcf8397df774414c0a7
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-07-18 17:42:41 +0000

    remake

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/AddCoinFragmentDialog.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/AddCoinFragmentDialog.kt
@@ -1,3 +1,8 @@
 package pawel.hn.coinmarketapp.fragments
 
-i
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.w
