4a75d7a53d47fd6881bdbdd77b577b7e86c907b6
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-08-17 19:22:19 +0000

    generate pdf

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/NewsFragment.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/NewsFragment.kt
@@ -9,4 +9,11 @@ import androidx.navigation.fragment.findNavController
 import com.prof.rssparser.Parser
 import pawel.hn.coinmarketapp.R
 import pawel.hn.coinmarketapp.adapters.NewsAdapter
-import pawel.hn.coinmarketapp.
+import pawel.hn.coinmarketapp.databinding.FragmentNewsBinding
+import pawel.hn.coinmarketapp.viewmodels.NewsViewModel
+
+
+
+class NewsFragment : Fragment(R.layout.fragment_news) {
+
+    private lateinit var ne
