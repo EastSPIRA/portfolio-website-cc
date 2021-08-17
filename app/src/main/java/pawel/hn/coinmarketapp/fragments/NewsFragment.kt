dcda79adf5018ffff9ba3a0dc0bcc49f121ca9e2
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-08-17 10:15:45 +0000

    deleted readme

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/NewsFragment.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/NewsFragment.kt
@@ -5,4 +5,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigat
+import androidx.navigation.fragment.findNavController
+import com.prof.rssparser.Parser
+import pawel.hn.coinmarketapp.R
+import pawel.hn.coinmarketapp.adapters.NewsAdapter
+import pawel.hn.coinmarketapp.
