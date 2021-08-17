a79d008599a0d0751a8492ec78461f95244c9023
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-08-17 09:32:11 +0000

    ASoC: sgtl5000: Fix SMALL_POP bit definition

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/NewsFragment.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/NewsFragment.kt
@@ -1,3 +1,8 @@
 package pawel.hn.coinmarketapp.fragments
 
-import android.conte
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigat
