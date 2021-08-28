f4b90fbeff3d7d8eedc9b1d616d6f77bdf5323c3
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-08-28 22:24:26 +0000

    More spacing

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/PriceNotifyFragment.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/PriceNotifyFragment.kt
@@ -4,4 +4,8 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Frag
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.preference.PreferenceManager
+import dagger.hilt.android.AndroidEntryPoint
+impo
