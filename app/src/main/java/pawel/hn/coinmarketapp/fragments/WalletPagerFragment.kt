b6fbaf2e5809c0ce1db034414ab2376e13f17f6e
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-09-22 22:03:02 +0000

    Refactor constants

--- app/src/main/java/pawel/hn/coinmarketapp/fragments/WalletPagerFragment.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/fragments/WalletPagerFragment.kt
@@ -3,4 +3,7 @@ package pawel.hn.coinmarketapp.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapte
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+import pawel.hn.coinmarketapp.R
+import pawel.hn.coinmarketapp.databinding.PageWalletBinding
