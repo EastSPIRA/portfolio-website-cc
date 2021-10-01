76fccb0d8b2da9795c5e9e03d08f95846adce77b
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-10-01 13:35:12 +0000

    merge in changes from master

--- app/src/main/java/pawel/hn/coinmarketapp/model/coinmarketcap/CoinData.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/model/coinmarketcap/CoinData.kt
@@ -1,4 +1,12 @@
 package pawel.hn.coinmarketapp.model.coinmarketcap
 
 import com.google.gson.annotations.SerializedName
-import pawel.hn.coinmarket
+import pawel.hn.coinmarketapp.database.Coin
+import pawel.hn.coinmarketapp.util.CURRENCY_PLN
+import pawel.hn.coinmarketapp.util.CURRENCY_USD
+
+data class CoinData(
+    @SerializedName("id")
+    val id: Int,
+
+  
