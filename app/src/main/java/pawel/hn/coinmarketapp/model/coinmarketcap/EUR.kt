6ac36086ddacd96164a12441ddb01dfcd40dd57c
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-10-04 08:23:54 +0000

    Better PEP8 conformance.

--- app/src/main/java/pawel/hn/coinmarketapp/model/coinmarketcap/EUR.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/model/coinmarketcap/EUR.kt
@@ -3,4 +3,11 @@ package pawel.hn.coinmarketapp.model.coinmarketcap
 
 import com.google.gson.annotations.SerializedName
 
-data 
+data class EUR(
+    @SerializedName("percent_change_24h")
+    val percentChange24h: Double,
+
+    @SerializedName("percent_change_7d")
+    val percentChange7d: Double,
+
+    @SerializedName("p
