553b5cbb3c8377211479c3c394369d5803774be2
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-10-10 15:50:51 +0000

    Delete style.scss

--- app/src/main/java/pawel/hn/coinmarketapp/model/coinmarketcap/USD.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/model/coinmarketcap/USD.kt
@@ -4,4 +4,9 @@ package pawel.hn.coinmarketapp.model.coinmarketcap
 import com.google.gson.annotations.SerializedName
 
 data class USD(
-    @SerializedName("percent_change_24
+    @SerializedName("percent_change_24h")
+    val percentChange24h: Double,
+    @SerializedName("percent_change_7d")
+    val percentChange7d: Double,
+    @SerializedName("price")
+    val price: Dou
