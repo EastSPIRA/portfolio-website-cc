88f53abc511a19cfbddbd5bb5585cdd03ba57593
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-01 18:02:46 +0000

    Script cleanup

--- app/src/main/java/pawel/hn/coinmarketapp/repository/CoinsRepository.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/repository/CoinsRepository.kt
@@ -1,3 +1,7 @@
 package pawel.hn.coinmarketapp.repository
 
-import pawel.hn.coinmarketapp.data.Coi
+import pawel.hn.coinmarketapp.data.CoinsData
+import pawel.hn.coinmarketapp.data.RemoteData
+import pawel.hn.coinmarketapp.data.WalletData
+import pawel.hn.coinmarketapp.database.Coin
+import pawel.hn.coinmarketapp.model.coinmarket
