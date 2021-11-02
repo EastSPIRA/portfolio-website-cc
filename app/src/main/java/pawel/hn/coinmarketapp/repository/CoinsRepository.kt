944ddb5b5d356e6747957f6ee5bfbeda91579450
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-02 20:54:48 +0000

    Upgraded to latest Rust-Empty

--- app/src/main/java/pawel/hn/coinmarketapp/repository/CoinsRepository.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/repository/CoinsRepository.kt
@@ -4,4 +4,10 @@ import pawel.hn.coinmarketapp.data.CoinsData
 import pawel.hn.coinmarketapp.data.RemoteData
 import pawel.hn.coinmarketapp.data.WalletData
 import pawel.hn.coinmarketapp.database.Coin
-import pawel.hn.coinmarketapp.model.coinmarket
+import pawel.hn.coinmarketapp.model.coinmarketcap.ApiResponseArray
+import pawel.hn.coinmarketapp.model.coinmarketcap.CoinData
+import pawel.hn.coinmarketapp.util.*
+import javax.inject.Inject
+
+
+class CoinsRe
