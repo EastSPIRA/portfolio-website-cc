750a02ccbac5311adfbe94b4d1b759d630b7c345
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-13 21:59:55 +0000

    Added eslint config

--- app/src/main/java/pawel/hn/coinmarketapp/util/Constants.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/util/Constants.kt
@@ -5,4 +5,13 @@ const val TAG = "PHN"
 const val API_QUERY_START = 1
 const val API_QUERY_LIMIT = 100
 const val CURRENCY_USD = "USD"
-const val CURRENCY_PLN = "PLN
+const val CURRENCY_PLN = "PLN"
+const val CURRENCY_EUR = "EUR"
+
+const val DATABASE_NAME = "coins"
+
+const val BASE_URL_COINS = "https://pro-api.coinmarketcap.com/"
+
+
+const val API_HEADER = "X-CMC_PRO_API_KEY:"
+
