b7b9a79ee06072a1ec951659062c772813bcc2c5
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-13 14:07:30 +0000

    updating bin

--- app/src/main/java/pawel/hn/coinmarketapp/util/Constants.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/util/Constants.kt
@@ -1,4 +1,8 @@
 package pawel.hn.coinmarketapp.util
 
 
-const val TAG = "PHN
+const val TAG = "PHN"
+const val API_QUERY_START = 1
+const val API_QUERY_LIMIT = 100
+const val CURRENCY_USD = "USD"
+const val CURRENCY_PLN = "PLN
