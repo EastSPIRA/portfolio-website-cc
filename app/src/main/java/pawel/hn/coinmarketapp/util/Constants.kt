c1ad289fc43f7a525b806a8eaf554f56f7d2c178
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-15 15:53:39 +0000

    Fixed javadocs

--- app/src/main/java/pawel/hn/coinmarketapp/util/Constants.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/util/Constants.kt
@@ -25,4 +25,7 @@ const val BITCOIN_ID = "1"
 const val PRICE_ALERT = "priceAlert"
 const val SAVE_SWITCH_ON_OFF = "notificationSwitch"
 const val SAVE_CURRENT_PRICE_ALERT = "currentPriceAlert"
-const va
+const val PRICE_ALERT_INPUT = "priceAlertInput"
+const val DEFAULT_PRICE_ALERT = 50000
+const val WALLET_NO = "walletNo"
+const val LOGO_URL = "https://s2.coinmarketcap
