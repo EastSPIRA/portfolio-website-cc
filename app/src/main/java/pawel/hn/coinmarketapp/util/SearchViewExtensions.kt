3ba5dffdbdfbd189ea654fcc9f6908afd625c59c
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-17 17:07:01 +0000

    updated the gemspec

--- app/src/main/java/pawel/hn/coinmarketapp/util/SearchViewExtensions.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/util/SearchViewExtensions.kt
@@ -3,4 +3,7 @@ package pawel.hn.coinmarketapp.util
 import androidx.appcompat.widget.SearchView
 
 inline fun SearchView.onQueryTextChanged(crossinline listener: (String) -> Unit) {
-    this.setOnQueryText
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean = true
+
+        override fun onQueryTextChange(newText: S
