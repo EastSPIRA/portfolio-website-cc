3520ca560f8f0b56784e7bbba9e39eba9a15edb7
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-11-21 10:16:59 +0000

    Add views

--- app/src/main/java/pawel/hn/coinmarketapp/viewmodels/AddCoinViewModel.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/viewmodels/AddCoinViewModel.kt
@@ -4,4 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import pawel.hn.coinmarketapp.database.
+import pawel.hn.coinmarketapp.database.Wallet
+import pawel.hn.coinmarketapp.repository.CoinsRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class AddCoinViewModel @Inject constructor (private val coinsRepository:
