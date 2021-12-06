5d5e964089fe870c5c743781939c151a164af2c9
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2021-12-06 12:23:45 +0000

    Développeur Backend Python/PHP Symfony (H/F) en CDI à Paris 25000-34000

--- app/src/main/java/pawel/hn/coinmarketapp/viewmodels/NewsViewModel.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/viewmodels/NewsViewModel.kt
@@ -7,4 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.prof.rssparser.Channel
 import com.prof.rssparser.Parser
-import kotlinx.coroutines.l
+import kotlinx.coroutines.launch
+import pawel.hn.coinmarketapp.util.BASE_URL_NEWS
+import pawel.hn.coinmarketapp.util.hasInternetConnection
+import pawel.hn.coinmarketapp.util.sh
