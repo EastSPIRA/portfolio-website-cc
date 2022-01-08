19d5dc7db76e112b701e238a4bdd08906c076a93
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2022-01-08 20:57:20 +0000

    chore: new translations

--- app/src/main/java/pawel/hn/coinmarketapp/viewmodels/WalletViewModel.kt
+++ app/src/main/java/pawel/hn/coinmarketapp/viewmodels/WalletViewModel.kt
@@ -6,4 +6,8 @@ import android.graphics.Typeface
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewModelScope
 import com.github.mikephil.charting.charts.PieChart
-import com.github.mikephil.charting.data.Pie
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import dagg
