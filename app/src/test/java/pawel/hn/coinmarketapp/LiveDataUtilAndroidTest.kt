8d11c75ef7eae11ce57df7c56d198f12aec42806
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-03-13 18:39:35 +0000

    libceph: add update_authorizer auth method

--- app/src/test/java/pawel/hn/coinmarketapp/LiveDataUtilAndroidTest.kt
+++ app/src/test/java/pawel/hn/coinmarketapp/LiveDataUtilAndroidTest.kt
@@ -3,4 +3,9 @@ package pawel.hn.coinmarketapp
 import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import java.util.concurrent.CountDown
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+/**
+ * Gets the value of a [LiveData] or waits for it to have one, with a ti
