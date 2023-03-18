37de4ef2880e97109da3406264413c8c72304888
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-03-18 10:54:44 +0000

    Fix review remarks

--- app/src/test/java/pawel/hn/coinmarketapp/LiveDataUtilAndroidTest.kt
+++ app/src/test/java/pawel/hn/coinmarketapp/LiveDataUtilAndroidTest.kt
@@ -21,4 +21,10 @@ fun <T> LiveData<T>.getOrAwaitValue(
 ): T {
     var data: T? = null
     val latch = CountDownLatch(1)
-    val observer = object : 
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+ 
