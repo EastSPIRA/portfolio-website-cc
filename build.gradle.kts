5a7960cf32424a00f17d32fb18275ac231d0497e
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-03-22 22:31:50 +0000

    Revert "SELinux: per-command whitelisting of ioctls"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 import org.gradle.internal.impldep.org.eclipse.jgit.lib.ObjectChecker.type
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-b
+buildscript {
+
+    repositories {
+        google()
+        jcenter(){
+            content {
+                includeModule("com.todkars", "shimmer-recyclerview")
+                in
