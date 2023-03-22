de001d22c9b04081e5e63e12d7effeb3a2a83716
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-03-22 09:57:33 +0000

    Proofread

--- app/src/test/java/pawel/hn/coinmarketapp/LiveDataUtilAndroidTest.kt
+++ app/src/test/java/pawel/hn/coinmarketapp/LiveDataUtilAndroidTest.kt
@@ -34,3 +34,9 @@ fun <T> LiveData<T>.getOrAwaitValue(
         afterObserve.invoke()
 
         // Don't wait indefinitely if the LiveData is not set.
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+
+    } finally {
+        this.removeOb
