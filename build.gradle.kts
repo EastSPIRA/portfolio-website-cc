8d2b58ff0d85b6e7c235d1a93386f79485b9e4f8
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-03-28 11:51:53 +0000

    Updates for release

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,4 +23,11 @@ buildscript {
 
         classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:${DependenciesVersions.navigationComponentVersion}")
 
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:${DependenciesVersions.hiltVe
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:${DependenciesVersions.hiltVersion}")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    re
