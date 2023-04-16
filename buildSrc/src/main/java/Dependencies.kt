4fed77ce219d5816f2c9c106523f5aec780aedf8
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-04-16 15:28:47 +0000

    updating translations

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,4 +4,6 @@ object Dependencies {
     const val kotlinKtx =  "androidx.core:core-ktx:${DependenciesVersions.kotlinKtxVersion}"
     const val appCompat = "androidx.appcompat:appcompat:${DependenciesVersions.appCompatVersion}"
     const val googleMaterial = "com.google.android.material:material:${DependenciesVersions.googleMaterialVersion}"
-    const val constraintLayout =  "androidx.constraintlayout:constraintlayout:${DependenciesVer
+    const val constraintLayout =  "androidx.constraintlayout:constraintlayout:${DependenciesVersions.constraintLayoutVersion}"
+    const val swipeToRefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0"
+    const val navi
