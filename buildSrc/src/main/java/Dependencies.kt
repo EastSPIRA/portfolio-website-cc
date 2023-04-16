ece801190c1ae878f5bd135a5d31b1131fd145d2
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-04-16 11:26:54 +0000

    fix documentation typo

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,4 +3,5 @@ object Dependencies {
     const val kotlinCore =  "org.jetbrains.kotlin:kotlin-stdlib:${DependenciesVersions.kotlinVersion}"
     const val kotlinKtx =  "androidx.core:core-ktx:${DependenciesVersions.kotlinKtxVersion}"
     const val appCompat = "androidx.appcompat:appcompat:${DependenciesVersions.appCompatVersion}"
-    const val googleMaterial = "com.google.android.mate
+    const val googleMaterial = "com.google.android.material:material:${DependenciesVersions.googleMaterialVersion}"
+    const val constraintLayout =  "androidx.constraintlayout:constraintlayout:${DependenciesVer
