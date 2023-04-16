530756de44a958d8298a1f494f86523fbfe8845e
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-04-16 08:03:25 +0000

    Changed font

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,3 +2,5 @@ object Dependencies {
 
     const val kotlinCore =  "org.jetbrains.kotlin:kotlin-stdlib:${DependenciesVersions.kotlinVersion}"
     const val kotlinKtx =  "androidx.core:core-ktx:${DependenciesVersions.kotlinKtxVersion}"
+    const val appCompat = "androidx.appcompat:appcompat:${DependenciesVersions.appCompatVersion}"
+    const val googleMaterial = "com.google.android.mate
