363e984f08ebf6b10ba76c5650eda0618ff5b422
EastSPIRA <96557909+EastSPIRA@users.noreply.github.com>
2023-05-28 20:21:32 +0000

    Add Travis file

--- gradlew.bat
+++ gradlew.bat
@@ -5,4 +5,10 @@
 @rem
 @rem ##########################################################################
 
-@rem Set loca
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set A
